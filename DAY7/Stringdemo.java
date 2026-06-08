0cf02a9ddc33946725265caeff11038daea81795
Your Name <you@example.com>
2026-06-08 13:23:04 +0530

    Add MergeSort implementation and String manipulation examples

---
+++ DAY7/Stringdemo.java
@@ -0,0 +1,46 @@
+package DAY7;
+public class Stringdemo{
+    public static void main(String[] args){
+        String str = "Swaraj";
+        System.out.println(str);
+        
+        String str2 = "Swaraj";
+        System.out.println(str2);
+        System.out.println(str == str2); // true, because both str and str2 refer to the same string literal in the string pool
+
+        String collegeName = new String("MIT");
+        System.out.println(collegeName);
+        String collegeName2 = new String("MIT");
+        System.out.println(collegeName2);
+        System.out.println(collegeName == collegeName2); // false, because str1 and str2 refer to different objects in the heap
+
+        String str3 = "Swaraj";
+        System.out.println(str3.hashCode());
+
+        System.out.println(str.length());
+        System.out.println(str.charAt(0));
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+
+
+        String str4 = "ironman";
+        System.out.println(str4.contains("man"));
+        System.out.println(str4.startsWith("iron"));
+        System.out.println(str4.endsWith("n"));
+
+        String str5 = "   Hello World!   ";
+        System.out.println(str5.substring(3, 8)); 
+        System.out.println(str5.replace("o","a"));
+
+        String str6 = "Java,Python,C++";
+        String[] subjects = str6.split(",");
+        for(String subject : subjects){
+            System.out.println(subject);
+        }
+        System.out.println(str5.trim());
+    
+    
+    
+    }
+
+}
